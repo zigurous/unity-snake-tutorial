3c762a1b388ced0910ec48f7006eb7baa53fef85
Adam Graham <adam@zigurous.com>
2023-11-11 20:08:30 -0600

    Implement snake segment orientations

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
-    public Transform segmentPrefab;
-    public Vector2Int direction = Vector2Int.right;
+    public SnakeSegment segmentPrefab;
     public float speed = 20f;
     public float speedMultiplier = 1f;
     public int initialSize = 4;
     public bool moveThroughWalls = false;
 
-    private readonly List<Transform> segments = new List<Transform>();
+    private readonly List<SnakeSegment> segments = new List<SnakeSegment>();
+    private SnakeSegment head;
     private Vector2Int input;
     private float nextUpdate;
+
+    private void Awake()
+    {
+        if (!TryGetComponent(out head))
+        {
+            head = gameObject.AddComponent<SnakeSegment>();
+            head.hideFlags = HideFlags.HideInInspector;
+        }
+    }
 
     private void Start()
     {
@@ -23,7 +32,7 @@ public class Snake : MonoBehaviour
     private void Update()
     {
         // Only allow turning up or down while moving in the x-axis
-        if (direction.x != 0f)
+        if (head.direction.x != 0f)
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
                 input = Vector2Int.up;
@@ -32,7 +41,7 @@ public class Snake : MonoBehaviour
             }
         }
         // Only allow turning left or right while moving in the y-axis
-        else if (direction.y != 0f)
+        else if (head.direction.y != 0f)
         {
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
                 input = Vector2Int.right;
@@ -51,21 +60,21 @@ public class Snake : MonoBehaviour
 
         // Set the new direction based on the input
         if (input != Vector2Int.zero) {
-            direction = input;
+            head.SetDirection(input, Vector2Int.zero);
         }
 
         // Set each segment's position to be the same as the one it follows. We
         // must do this in reverse order so the position is set to the previous
         // position, otherwise they will all be stacked on top of each other.
         for (int i = segments.Count - 1; i > 0; i--) {
-            segments[i].position = segments[i - 1].position;
+            segments[i].Follow(segments[i - 1], i, segments.Count);
         }
 
         // Move the snake in the direction it is facing
         // Round the values to ensure it aligns to the grid
-        int x = Mathf.RoundToInt(transform.position.x) + direction.x;
-        int y = Mathf.RoundToInt(transform.position.y) + direction.y;
-        transform.position = new Vector2(x, y);
+        int x = Mathf.RoundToInt(head.transform.position.x) + head.direction.x;
+        int y = Mathf.RoundToInt(head.transform.position.y) + head.direction.y;
+        head.transform.position = new Vector2(x, y);
 
         // Set the next update time based on the speed
         nextUpdate = Time.time + (1f / (speed * speedMultiplier));
@@ -73,15 +82,15 @@ public class Snake : MonoBehaviour
 
     public void Grow()
     {
-        Transform segment = Instantiate(segmentPrefab);
-        segment.position = segments[segments.Count - 1].position;
+        SnakeSegment segment = Instantiate(segmentPrefab);
+        segment.Follow(segments[segments.Count - 1], segments.Count, segments.Count + 1);
         segments.Add(segment);
     }
 
     public void ResetState()
     {
-        direction = Vector2Int.right;
-        transform.position = Vector3.zero;
+        head.SetDirection(Vector2Int.right, Vector2Int.zero);
+        head.transform.position = Vector3.zero;
 
         // Start at 1 to skip destroying the head
         for (int i = 1; i < segments.Count; i++) {
@@ -90,7 +99,7 @@ public class Snake : MonoBehaviour
 
         // Clear the list but add back this as the head
         segments.Clear();
-        segments.Add(transform);
+        segments.Add(head);
 
         // -1 since the head is already in the list
         for (int i = 0; i < initialSize - 1; i++) {
@@ -100,10 +109,10 @@ public class Snake : MonoBehaviour
 
     public bool Occupies(int x, int y)
     {
-        foreach (Transform segment in segments)
+        foreach (SnakeSegment segment in segments)
         {
-            if (Mathf.RoundToInt(segment.position.x) == x &&
-                Mathf.RoundToInt(segment.position.y) == y) {
+            if (Mathf.RoundToInt(segment.transform.position.x) == x &&
+                Mathf.RoundToInt(segment.transform.position.y) == y) {
                 return true;
             }
         }
@@ -135,10 +144,10 @@ public class Snake : MonoBehaviour
     {
         Vector3 position = transform.position;
 
-        if (direction.x != 0f) {
-            position.x = Mathf.RoundToInt(-wall.position.x + direction.x);
-        } else if (direction.y != 0f) {
-            position.y = Mathf.RoundToInt(-wall.position.y + direction.y);
+        if (head.direction.x != 0f) {
+            position.x = Mathf.RoundToInt(-wall.position.x + head.direction.x);
+        } else if (head.direction.y != 0f) {
+            position.y = Mathf.RoundToInt(-wall.position.y + head.direction.y);
         }
 
         transform.position = position;
---
+++ Assets/Scripts/SnakeSegment.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SnakeSegment : MonoBehaviour
+{
+    private static Dictionary<Vector2Int, Dictionary<Vector2Int, float>> orientations;
+
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite head;
+    public Sprite tail;
+    public Sprite body;
+    public Sprite corner;
+
+    public Vector2Int direction { get; private set; }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetDirection(Vector2Int direction, Vector2Int previous)
+    {
+        // Initialize the orientations for the first time if not yet set
+        if (orientations == null) {
+            InitializeOrientations();
+        }
+
+        // Calculate the correct rotation based on the direction (new and old)
+        float angle = orientations[direction][previous];
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Set the new direction
+        this.direction = direction;
+    }
+
+    public void Follow(SnakeSegment other, int index, int length)
+    {
+        // Determine if the segment is the head, tail, or a turning segment
+        bool isHead = index == 0;
+        bool isTail = index == length - 1;
+        bool isTurning = direction != other.direction;
+
+        // Set the correct sprite depending where the segment is in the snake
+        if (index == 0) {
+            spriteRenderer.sprite = head;
+        } else if (index == length - 1) {
+            spriteRenderer.sprite = tail;
+        } else if (isTurning) {
+            spriteRenderer.sprite = corner;
+        } else {
+            spriteRenderer.sprite = body;
+        }
+
+        // The head and tail segments should never be considered turning since
+        // the rotation would not match up to the corner pieces
+        if (isTurning && !isHead && !isTail) {
+            SetDirection(other.direction, direction);
+        } else {
+            SetDirection(other.direction, Vector2Int.zero);
+        }
+
+        // Match the position of the segment to the one its following
+        transform.position = other.transform.position;
+    }
+
+    private void InitializeOrientations()
+    {
+        orientations = new Dictionary<Vector2Int, Dictionary<Vector2Int, float>>(5)
+        {
+            { Vector2Int.zero, new Dictionary<Vector2Int, float>(5) },
+            { Vector2Int.right, new Dictionary<Vector2Int, float>(5) },
+            { Vector2Int.up, new Dictionary<Vector2Int, float>(5) },
+            { Vector2Int.left, new Dictionary<Vector2Int, float>(5) },
+            { Vector2Int.down, new Dictionary<Vector2Int, float>(5) }
+        };
+
+        orientations[Vector2Int.zero].Add(Vector2Int.zero, 0.0f);
+        orientations[Vector2Int.zero].Add(Vector2Int.right, 0.0f);
+        orientations[Vector2Int.zero].Add(Vector2Int.up, 90.0f);
+        orientations[Vector2Int.zero].Add(Vector2Int.left, 180.0f);
+        orientations[Vector2Int.zero].Add(Vector2Int.down, 270.0f);
+
+        orientations[Vector2Int.right].Add(Vector2Int.zero, 0.0f);
+        orientations[Vector2Int.right].Add(Vector2Int.left, 0.0f);
+        orientations[Vector2Int.right].Add(Vector2Int.right, 0.0f);
+        orientations[Vector2Int.right].Add(Vector2Int.down, 0.0f);
+        orientations[Vector2Int.right].Add(Vector2Int.up, -90.0f);
+
+        orientations[Vector2Int.up].Add(Vector2Int.zero, 90.0f);
+        orientations[Vector2Int.up].Add(Vector2Int.up, 90.0f);
+        orientations[Vector2Int.up].Add(Vector2Int.down, 90.0f);
+        orientations[Vector2Int.up].Add(Vector2Int.right, 90.0f);
+        orientations[Vector2Int.up].Add(Vector2Int.left, 0.0f);
+
+        orientations[Vector2Int.left].Add(Vector2Int.zero, 180.0f);
+        orientations[Vector2Int.left].Add(Vector2Int.left, 180.0f);
+        orientations[Vector2Int.left].Add(Vector2Int.right, 180.0f);
+        orientations[Vector2Int.left].Add(Vector2Int.up, 180.0f);
+        orientations[Vector2Int.left].Add(Vector2Int.down, 90.0f);
+
+        orientations[Vector2Int.down].Add(Vector2Int.zero, 270.0f);
+        orientations[Vector2Int.down].Add(Vector2Int.down, 270.0f);
+        orientations[Vector2Int.down].Add(Vector2Int.up, 270.0f);
+        orientations[Vector2Int.down].Add(Vector2Int.left, 270.0f);
+        orientations[Vector2Int.down].Add(Vector2Int.right, 180.0f);
+    }
+
+}
